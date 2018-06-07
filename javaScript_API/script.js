7c5e75a59cfb4ea8dff774dcb30e7d461fa63f26
Chris DEV <softreaver@laposte.net>
2018-06-07 17:00:46 +0200

    MAJ du 07/06/2018

--- javaScript_API/script.js
+++ javaScript_API/script.js
@@ -2,13 +2,19 @@
 //Data pool
 let stations;                                                               //tableau qui contiendra la liste de toutes les stations vélos de Toulouse
 let addressList             = [];                                           //Tableau qui contiendra les adresses des stations vélo
+let markerList              = [];                                           //Liste des marker à afficher sur la Google map
+let infoWindowList          = [];                                           //Liste des infobulle pour les marqueurs
 const suggestionsListElt    = document.getElementById("suggestions");       //La boite proposant les suggestions d'adresse
 const searchInputElt        = document.getElementById('address');           //Le champ permettant d'entrer la recherche d'adresse
 let firstLoad               = true;                                         //Un booleen indiquant si la page vient d'être chargée ou pas
 const middleContainerElt    = document.getElementById("middle-container");  //Element <section>
 const stationsList          = document.getElementById("stations-list");     //Element contenant la liste des stations recherchées
 const stationInfo           = document.getElementById("station-info");      //Element contenant les informations de la station delectionnée
-let stationsToDisplay       = [];                                           //Tableau contenant tous les indexs des stations à afficher
+let map;                                                                    //Objet contenant la Google map
+let googleMapReady          = false                                         //Booleen indiquant si la Google map à bien finit de s'initialiser
+let stationsListReady       = false                                         //Booleen indiquant si la liste des stations de vélo à bien finit de se télécharger
+let firstInitMarker         = true                                          //Booleen indiquant si les marqueur on déjà été initialisés une première fois
+let openedInfoWindow        = -1                                            //Contient l'ID de la fenêtre actuellement ouverte, sinon vaut -1.
 
 //Données du service en ligne
 const APIUrl            = "https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=";
@@ -42,7 +48,13 @@ function upDate(){
             firstLoad = false;
         }
 
-        displayStations(stationsToDisplay);
+        displayStations();
+
+        //On signal que la liste des staions à bien été mise à jour
+        stationsListReady = true;
+
+        // Si la carte et la liste des stations sont initialisées, on place les marqueurs sur la carte
+        if(isAllReady) initMarker();
     
     }, function(error){
         //Afficher le message d'erreur sur la page
@@ -74,7 +86,7 @@ function suggest(event){
 
                 //Puis la redimentionne comme il faut
                 suggestionsListElt.style.maxHeight  = "50vh";
-                suggestionsListElt.style.width      = "100%";
+                suggestionsListElt.style.width      = "60vw";
             }
 
             let suggestionElt = document.createElement("div");
@@ -111,22 +123,12 @@ searchInputElt.addEventListener("focus", suggest);
  * Afficher les stations
 ***************************************/
 
-function displayStations(list){
+function displayStations(){
     for(let i = 0; i < stations.length; i++){
-        if(list.length > 0){
-            list.forEach(function(elem){
-                if(elem === i)
-                    stationsList.insertAdjacentHTML(
-                        "beforeend",
-                        `<div id="${ i }">${ stations[i].name }</div>`
-                    );
-            });
-        }else{
-            stationsList.insertAdjacentHTML(
-                "beforeend",
-                `<div id="${ i }">${ stations[i].name }</div>`
-            );
-        }
+        stationsList.insertAdjacentHTML(
+            "beforeend",
+            `<div id="${ i }" class= "station">${ stations[i].name }</div>`
+        );
     }
 }
 
@@ -156,3 +158,104 @@ function flushSuggestions(){
     suggestionsListElt.style.visibility = "hidden";
 }
 
+/*************************************
+ * Initialisation de la Google map
+***************************************/
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 43.60, lng: 1.455},
+    zoom: 13,
+    });
+    
+    // On signal que la GoogleMap a finit de s'initialiser
+    googleMapReady = true;
+
+    //On initialise les marqueurs si la carte et la listes des staions sont bien initialisés
+    if(isAllReady) initMarker();
+}
+
+/*************************************
+ * Initialisation et placement des
+ * marqueurs sur la Google map
+***************************************/
+
+function initMarker(){
+    //Initialisation de la liste des marker
+    infoWindowList = [];
+    let i = 0;
+    for(let station of stations){
+        //Initialisation des données à mettre dans le markeur
+        let myLatlng = station.position
+        //création d'un infobulle
+        //Borne de paiement disponible à la station
+        let banking = (station.banking) ? "OUI" : "NON" ;
+
+        //Disponibilité de la station
+        let status = (station.status === "OPEN") ? 
+            '<span class="open">OUVERT</span>' :
+            '<span class="closed">FERMÉE</span>' ;
+
+        //Dernière mise à jour des données
+        let lastUpdate = "A FAIRE";
+
+            //Contenu de l'infobulle
+        let infoWindowContent = `
+        <div id="infoBulle-container">
+            <h3>${ station.name } N° ${ station.number }</h3>
+            <div id="infoBulle-content">
+                <p>${ status }</p>
+                <p>Dernière mise à jour il y a ${ lastUpdate }</p>
+                <p>Vélos dispo : ${ station.available_bikes }</p>
+                <p>Place dispo : ${ station.available_bike_stands }</p>
+                <p>Borne de paiement : ${ banking }</p>
+                <p><span class="small">lat : ${ station.position.lat } - long : ${ station.position.lng }</span></p>
+            </div>
+        </div>`;
+
+        infoWindowList.push(
+            new google.maps.InfoWindow({
+                content: infoWindowContent
+            })
+        );
+
+        let marker = new google.maps.Marker({
+                position: myLatlng,
+                //Si c'est la première fois qu'on les initialise, on leur ajoute une animation
+                animation: (firstInitMarker) ? google.maps.Animation.DROP : "",
+                IDInfoWindow: i
+            })
+
+        markerList.push(marker);
+        marker.addListener("click", function(event){
+            //Fermer la fenêtre déjà ouverte
+            if(openedInfoWindow !== -1) infoWindowList[openedInfoWindow].close();
+            infoWindowList[this.IDInfoWindow].open(map, this);
+            openedInfoWindow = this.IDInfoWindow;
+        });
+
+        //
+
+        //Incrémentation de l'itérateur
+        i++;
+    }
+
+    //Affichage des marqueurs sur la carte
+    for(let i = 0; i < markerList.length; i++){
+        setTimeout(
+            markerList[i].setMap(map),
+            200 * i
+        );
+    }
+
+    firstInitMarker = false;
+}
+
+/*************************************
+ * r'envoie TRUE si la carte ET la
+ * liste des stations sont à jours
+***************************************/
+
+function isAllReady(){
+    return stationsListReady && googleMapReady;
+}
